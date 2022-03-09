5fde048f8988c07b7a23aea01276491b275f367a
Nikola Culic <43279231+Culic96@users.noreply.github.com>
2022-03-09 19:17:14 +0100

    Creating basic concept for pizza Order page (#15)

--- src/pages/PizzaOrder/index.jsx
+++ src/pages/PizzaOrder/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { PageContainer, OrderDetails, OrderImage } from './style';
 
 const PizzaOrder = () => {
   return (
     <>
-      <div>Hello World</div>
+      <PageContainer>
+        <OrderImage imageUrl="../images/tuna.jpg"></OrderImage>
+        <OrderDetails>
+          <h3>Hot and Spicy!</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptas aut,
+            corporis rerum labore, eius perspiciatis tempore quos veniam beatae odio
+            nostrum quibusdam laboriosam consequuntur repellendus quia, possimus
+            sapiente? Expedita! Dolores ipsa quod iusto vel numquam. Aliquid
+            dignissimos, modi eaque sapiente dolor at porro facere voluptatem
+            consequuntur minus fuga reiciendis ipsa est, aliquam asperiores hic
+            possimus sed iure earum dolorum.
+          </p>
+          <h4>Price: 12$</h4>
+        </OrderDetails>
+      </PageContainer>
     </>
   );
 };
--- src/pages/PizzaOrder/style.jsx
+++ src/pages/PizzaOrder/style.jsx
@@ -1,1 +1,45 @@
 import styled from 'styled-components';
+
+export const PageContainer = styled.div({
+  height: '100%',
+  width: '100%',
+  display: 'flex',
+});
+
+export const OrderImage = styled.div(
+  {
+    textAlign: 'center',
+    float: 'left',
+    height: '76vh',
+    width: '45vw',
+    margin: '5rem 0 2rem 2rem',
+  },
+  ({ imageUrl }) => ({
+    background: `url(${imageUrl})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center center',
+    backgroundSize: 'cover',
+  })
+);
+
+export const OrderDetails = styled.div({
+  margin: '5rem 0 2rem 2rem',
+  height: '100vh',
+  width: '45vw',
+  h3: {
+    textAlign: 'left',
+    fontSize: '5rem',
+    fontWeight: '200',
+    marginBottom: '2rem',
+  },
+  p: {
+    marginTop: '2rem',
+    fontSize: '1.2rem',
+    fontWeight: '100',
+  },
+  h4: {
+    marginTop: '2rem',
+    fontSize: '2rem',
+    fontWeight: '400',
+  },
+});

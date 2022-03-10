ce1d3b5077b0517716ebfdf127142d6b8ab9a6f9
Nikola Culic <43279231+Culic96@users.noreply.github.com>
2022-03-10 12:40:22 +0100

    Footer style and minor fixes (#16)

--- src/components/Footer/style.jsx
+++ src/components/Footer/style.jsx
@@ -1,30 +1,50 @@
 import styled from 'styled-components';
 
 export const FooterLine = styled.hr({
-  border: '1px solid #ED1C24',
+  border: '2px solid #ED1C24',
   width: '95vw',
   marginBottom: '2rem',
 });
 
 export const FooterContainer = styled.div({
-  padding: '3rem',
+  padding: '2.5rem',
+  textAlign: 'center',
+  color: 'red',
 });
 
 export const FooterFlex = styled.div({
   display: 'flex',
+  textAlign: 'left',
+  p: {
+    margin: '0.5rem 0',
+    color: 'red',
+  },
 });
 
 export const FooterMenu = styled.div({
-  marginLeft: '10rem',
+  marginLeft: '5rem',
 });
 
 export const FooterLinks = styled.div({
+  height: '50%',
   display: 'flex',
   flexDirection: 'column',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  a: {
+    textDecoration: 'none',
+    fontSize: '1.2rem',
+    color: 'red',
+    // marginBottom: '1rem',
+  },
 });
 
 export const FooterSocial = styled.div({
-  marginTop: '7rem',
+  display: 'flex',
+  width: '150px',
+  marginTop: '4rem',
+  marginBottom: '4rem',
+  justifyContent: 'space-evenly',
 });
 
 export const FooterLogo = styled.img({
